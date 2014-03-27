738aae85ea9787f1eda31e19f9e37cc4a4b695bb
Marc Abramowitz <marc@marc-abramowitz.com>
2014-03-27 13:33:34 -0700

    setup.py: Add some metadata that seems to be required now

--- setup.py
+++ setup.py
@@ -4,4 +4,7 @@ from distutils.core import setup
 setup(
     name='capturemock_test_sqlalchemy',
     version="trunk",
+    url="https://github.com/msabramo/capturemock_test_sqlalchemy",
+    author="Marc Abramowitz",
+    author_email="msabramo@gmail.com",
 )
